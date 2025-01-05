ff1dc8a2627cca57080b7c47e592dc88d30cbd3f
Ulises <ulyses.sosa@gmail.com>
2025-01-05 10:31:10 +0100

    add know server tests using config file

--- src/tests/chat_test.rs
+++ src/tests/chat_test.rs
@@ -16,8 +16,38 @@ mod chat_test {
     use rustafarian_client::client::Client;
 
     #[test]
-    fn initialization_test() {
-        
-    }
+    fn initialization_test() {}
 
+    #[test]
+    fn known_servers() {
+        let chat_client_id = 4;
+        let simulation_controller =
+            SimulationController::build("src/tests/configurations/known_servers_test.toml", false);
+        let client_command_channel = simulation_controller
+            .nodes_channels
+            .get(&chat_client_id)
+            .unwrap()
+            .send_command_channel
+            .clone();
+        let client_response_channel = simulation_controller
+            .nodes_channels
+            .get(&chat_client_id)
+            .unwrap()
+            .receive_response_channel
+            .clone();
+        //send topology request
+        let res = client_command_channel.send(SimControllerCommand::KnownServers);
+        assert!(res.is_ok());
+        // Listen for topology response
+        for response in client_response_channel.iter() {
+            if let SimControllerResponseWrapper::Message(SimControllerMessage::KnownServers(
+                known_servers,
+            )) = response
+            {
+                println!("TEST - Known servers response {:?}", known_servers);
+                assert_eq!(known_servers.len(), 1);
+                break;
+            }
+        }
+    }
 }
